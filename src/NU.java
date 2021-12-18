51d9ec3e0dd3ba1d8d75897ed6674377908fb521
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-12-18 10:53:22 +0700

    Update NU.java

--- src/NU.java
+++ src/NU.java
@@ -54,7 +54,7 @@ public class NU extends Product {
     }
 
     public void Xuat() {
-        System.out.printf("%-10s %-15s %-25s %-10s %-10s", ThongtinSP.getStt(), IdNU, ThongtinSP.getTen(), ThongtinSP.getChiTietSP(),
+        System.out.printf("\n%-20s %-25s %-50s %-20s\n", IdNU, ThongtinSP.getTen(), ThongtinSP.getChiTietSP(),
                 Gia);
     }
 
