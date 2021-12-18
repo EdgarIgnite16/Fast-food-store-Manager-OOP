e7a28191fc5ac917bbb206df728ecbba0d11b0df
RumiAxolotl <57629251+RumiAxolotl@users.noreply.github.com>
2021-12-18 08:55:39 +0700

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,14 +1,71 @@
-import java.util.regex.Matcher;
+import java.util.Scanner;
 
 public class Product {
-    private String Ten;
-    private String ChiTietSP;
+    protected String Ten;
+    protected String ChiTietSP;
     private int stt = 1;
     private static int count = 0;
 
+    Scanner sc = new Scanner(System.in);
+
     public Product() {
-       this.Ten = null;
-       this.ChiTietSP = null;
-       this.stt = count++;
+        this.Ten = null;
+        this.ChiTietSP = null;
+        this.stt = count++;
     }
+
+    public Product(String Ten, String ChiTietSP) {
+        this.Ten = Ten;
+        this.ChiTietSP = ChiTietSP;
+        this.stt = count++;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+    public String getChiTietSP() {
+        return ChiTietSP;
+    }
+
+    public void setChiTietSP(String ChiTietSP) {
+        this.ChiTietSP = ChiTietSP;
+    }
+
+    public int getStt() {
+        return stt;
+    }
+
+    public void setStt(int stt) {
+        this.stt = stt;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Product.count = count;
+    }
+
+
+
+    public void Nhap() {
+
+        System.out.println("Nhap ten san pham: ");
+        Ten = sc.nextLine();
+
+        System.out.println("Mo ta san pham (gia, co, toping,...): ");
+        ChiTietSP = sc.nextLine();
+    }
+
+
+    public void Xuat() {
+        System.out.printf("%-10s %-25s %-15s\n", stt, Ten, ChiTietSP);
+    }
+
 }
